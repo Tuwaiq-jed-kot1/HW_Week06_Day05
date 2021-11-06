a9e939b735a44bf6479fe36d1b7aec78f4e99bf5
husam5009 <husam5009@hotmail.com>
2021-11-06 19:48:52 +0300

    Done, later i will do the Extra

---
+++ app/src/main/java/com/sumaya/hw_week06_day05/data/models/TheMovieData.kt
@@ -0,0 +1,12 @@
+package com.sumaya.hw_week06_day05.data.models
+
+data class TheMovieData(
+    val results: List<Results>
+)
+
+data class Results(
+    val poster_path: String,
+    val release_date: String,
+    val title: String,
+    val vote_average: Double,
+)
