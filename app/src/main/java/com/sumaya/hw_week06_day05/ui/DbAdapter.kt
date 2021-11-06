b148126262d02f3a4cddc348b97dbc5c3d831f42
Fatim <Fatimah.Alsuhaymi.jed.@tuwaiq.edu.sa>
2021-11-07 00:06:44 +0300

    homework

--- app/src/main/java/com/sumaya/hw_week06_day05/ui/DbAdapter.kt
+++ app/src/main/java/com/sumaya/hw_week06_day05/ui/DbAdapter.kt
@@ -11,31 +11,31 @@ import com.sumaya.hw_week06_day05.R
 import com.sumaya.hw_week06_day05.data.modle.DbData
 import com.sumaya.hw_week06_day05.data.modle.Results
 
-    class DbAdapter(val Movies: List<Results>) : RecyclerView.Adapter<customHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): customHolder {
-            val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.recycle_movie_item, parent, false)
+class DbAdapter(val Movies: List<Results>) : RecyclerView.Adapter<customHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): customHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recycle_movie_item, parent, false)
 
-            return customHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: customHolder, position: Int) {
-            val movie = Movies[position]
-            holder.Title.text = movie.title
-            holder.Date.text = movie.release_date
-            holder.imageIV.load("https://image.tmbd.org/t/p/w500"+movie.poster_path)
-            holder.VoteAverage.text=movie.vote_average.toString()
-        }
-
-        override fun getItemCount(): Int {
-            return Movies.size
-        }
+        return customHolder(view)
     }
 
-    class customHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val Title: TextView = itemView.findViewById(R.id.title_movie)
-        val imageIV: ImageView = itemView.findViewById(R.id.poster)
-        val Date: TextView = itemView.findViewById(R.id.date)
-        val VoteAverage: TextView = itemView.findViewById(R.id.vote_average)
+    override fun onBindViewHolder(holder: customHolder, position: Int) {
+        val movie = Movies[position]
+        holder.Title.text = movie.title
+        holder.Date.text = movie.release_date
+        holder.imageIV.load("https://image.tmdb.org/t/p/w500/" + movie.poster_path)
+        holder.VoteAverage.text = movie.vote_average.toString()
     }
+
+    override fun getItemCount(): Int {
+        return Movies.size
+    }
+}
+
+class customHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val Title: TextView = itemView.findViewById(R.id.title_movie)
+    val imageIV: ImageView = itemView.findViewById(R.id.poster)
+    val Date: TextView = itemView.findViewById(R.id.date)
+    val VoteAverage: TextView = itemView.findViewById(R.id.vote_average)
+}
 
--- app/src/main/java/com/sumaya/hw_week06_day05/ui/MainActivity.kt
+++ app/src/main/java/com/sumaya/hw_week06_day05/ui/MainActivity.kt
@@ -12,60 +12,54 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.sumaya.hw_week06_day05.R
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var recyclerMovies : RecyclerView
+    private lateinit var recyclerMovies: RecyclerView
 
     private val vm by lazy {
         ViewModelProvider(this).get(MainViewModel::class.java)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerMovies = findViewById(R.id.DBmovies)
-        recyclerMovies.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        recyclerMovies.layoutManager =
+            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
 
         loadMoviesList()
     }
-    private fun loadMoviesList(query:String? =null) {
+
+    private fun loadMoviesList(query: String? = null) {
 
         vm.fetchMoviesList(query).observe(this, {
-         if (query.isNullOrEmpty()) {
-             recyclerMovies.adapter = DbAdapter(it.results)
-         }else {
-             recyclerMovies.swapAdapter(DbAdapter(it.results),false)
-         }
+            if (query.isNullOrEmpty()) {
+                recyclerMovies.adapter = DbAdapter(it.results)
+            } else {
+                recyclerMovies.swapAdapter(DbAdapter(it.results), false)
+            }
 
         })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.search_mune,menu)
-        val tag ="search view"
+        menuInflater.inflate(R.menu.search_mune, menu)
+        val tag = "search view"
         val searchIcon: MenuItem = menu!!.findItem(R.id.app_bar_search)
         val searchView = searchIcon.actionView as SearchView
 
 
         searchView.apply {
-// search view
             setOnQueryTextListener(object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String?): Boolean {
-                    Log.d(tag,"Query Text submit $query")
+                    Log.d(tag, "Query Text submit $query")
                     loadMoviesList(query)
                     return true
-
                 }
-
-
 
                 override fun onQueryTextChange(newText: String?): Boolean {
-
-                    Log.d(tag,"Query Text Cheange $newText")
-
+                    Log.d(tag, "Query Text Cheange $newText")
                     return false
                 }
-
-
             })
-
         }
         return super.onCreateOptionsMenu(menu)
     }
