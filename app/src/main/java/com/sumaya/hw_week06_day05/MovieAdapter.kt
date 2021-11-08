10e76374b2cfcbe78878be3d9f36af48ec0c2c4b
asmaa <Asma.Alghamdi.jed.k1@tuwaiq.edu.sa>
2021-11-09 00:46:26 +0300

    W7-D2

--- app/src/main/java/com/sumaya/hw_week06_day05/MovieAdapter.kt
+++ app/src/main/java/com/sumaya/hw_week06_day05/MovieAdapter.kt
@@ -1,9 +1,13 @@
 package com.sumaya.hw_week06_day05
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.sumaya.hw_week06_day05.WebViewActivity
+
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import coil.size.Scale
@@ -22,6 +26,15 @@ class MovieAdapter(private val movies: List<movie>) : RecyclerView.Adapter<Custo
         holder.vote_average.text=movie.vote_average.toString()
         holder.Image.load("https://image.tmdb.org/t/p/original/"+movie.poster_path){
             scale(Scale.FILL)
+        }
+
+        holder.itemView.setOnClickListener{
+            val intent= Intent( holder.itemView.context, WebViewActivity::class.java).apply {
+              putExtra("webKey",movie.id.toString())
+            }
+            holder.itemView.context.startActivity(intent)
+
+
         }
     }
 
