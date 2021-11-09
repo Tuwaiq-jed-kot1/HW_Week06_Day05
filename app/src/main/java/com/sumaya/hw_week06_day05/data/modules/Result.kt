3aad6985594e9cb150cdce8a4fdb988b53f63acd
RanaJaberAlrajhi <Rana.Alrajhi.jed.k1@tuwaiq.edu.sa>
2021-11-09 08:35:59 +0300

    Done..

--- app/src/main/java/com/sumaya/hw_week06_day05/data/modules/Result.kt
+++ app/src/main/java/com/sumaya/hw_week06_day05/data/modules/Result.kt
@@ -1,6 +1,7 @@
 package com.sumaya.hw_week06_day05.data.modules
 
 data class Result (
+    val id : Int,
     val poster_path : String,
     val release_date : String,
     val title : String,
--- app/src/main/java/com/sumaya/hw_week06_day05/ui/MoviesAdapter.kt
+++ app/src/main/java/com/sumaya/hw_week06_day05/ui/MoviesAdapter.kt
@@ -1,17 +1,22 @@
 package com.sumaya.hw_week06_day05.ui
 
+import android.content.Intent
+import android.icu.number.Scale
+import android.view.Gravity.FILL
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout.FILL
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.sumaya.hw_week06_day05.R
 import com.sumaya.hw_week06_day05.data.modules.Result
+import com.sumaya.hw_week06_day05.data.network.MoviesWebView
 
 
-class MoviesAdapter(val moviesData: List<Result>) : RecyclerView.Adapter<CustomHolder>() {
+class MoviesAdapter( val moviesData: List<Result>) : RecyclerView.Adapter<CustomHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item, parent, false)
@@ -24,6 +29,16 @@ class MoviesAdapter(val moviesData: List<Result>) : RecyclerView.Adapter<CustomH
         holder.dateTV.text = movies.release_date
         holder.votesTV.text = movies.vote_average.toString()
         holder.imageIV.load("https://image.tmdb.org/t/p/original/"+ movies.poster_path)
+
+        holder.imageIV.setOnClickListener{
+            val intent= Intent( holder.itemView.context, MoviesWebView::class.java).apply {
+                putExtra("mKey",movies.id.toString())
+
+            }
+            holder.itemView.context.startActivity(intent)
+
+
+        }
     }
 
     override fun getItemCount(): Int {
@@ -36,4 +51,6 @@ class CustomHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val dateTV: TextView = itemView.findViewById(R.id.tvDate)
     val votesTV: TextView = itemView.findViewById(R.id.tvVotes)
     val imageIV: ImageView = itemView.findViewById(R.id.ivPoster)
+
+
 }
